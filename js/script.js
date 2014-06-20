23431ee7b8a52e91d42f6821cba1780c16a240aa
Tim Licata <tlicata@hotpads.com>
2014-06-20 13:22:47 -0700

    movie <li>s are links that lookup details by imdbID

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var OMDB_URL = "http://www.omdbapi.com/";
+
 var errorDOM = $("#error-message");
 var resultsDOM = $("#search-results");
 
@@ -14,8 +16,16 @@ var renderMovieResults = function (results) {
     clearPreviousResults();
     $.each(results.Search, function (index, movie) {
         var text = movie.Title + " (" + movie.Year + ")";
-        resultsDOM.append($("<li/>").text(text));
+        var url = "http://www.imdb.com/title/" + movie.imdbID;
+        var link = $("<a/>").text(text).attr("href", url);
+        link.data("imdbID", movie.imdbID);
+        var item = $("<li/>").append(link);
+        resultsDOM.append(item);
     });
+};
+
+var renderDetails = function (details) {
+    console.log("renderDetails", details);
 };
 
 var renderError = function (message) {
@@ -23,15 +33,29 @@ var renderError = function (message) {
     errorDOM.append(message || "Oops, an error occurred");
 };
 
+var lookupMovieId = function (id) {
+    $.ajax({
+        data: {"i": id},
+        dataType: "jsonp",
+        url: OMDB_URL,
+        success: renderDetails
+    });
+};
 var searchForMovie = function (searchText) {
     $.ajax({
         data: {"s": searchText},
         dataType: "jsonp",
-        url: "http://www.omdbapi.com/",
+        url: OMDB_URL,
         success: renderMovieResults,
         error: renderError
     });
 };
+
+resultsDOM.on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).data("imdbID");
+    lookupMovieId(id);
+});
 
 $("#movie-search-form").on("submit", function (event) {
     event.preventDefault();
