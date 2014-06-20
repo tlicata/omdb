d6d468b84a50dac8e4d28cf7dfd3b3859abd41ff
Tim Licata <tlicata@hotpads.com>
2014-06-20 11:24:00 -0700

    create ajax call for hitting omdb api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+var renderMovieResults = function (results) {
+    console.log("rendering results", results);
+};
+
+var renderError = function () {
+    console.log("oops, an error occurred");
+};
+
+var searchForMovie = function (searchText) {
+    $.ajax({
+        data: {"s": searchText},
+        dataType: "jsonp",
+        url: "http://www.omdbapi.com/",
+        success: renderMovieResults,
+        error: renderError
+    });
+};
+
 $("#movie-search-form").on("submit", function (event) {
     event.preventDefault();
     var query = $("#movie-search-query").val();
+    searchForMovie(query);
 });
